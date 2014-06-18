a7346e4634af4911bf6d98a35302871a16ad4702
Zach Abbott <zachary.abbott@banno.com>
2014-06-17 23:05:19 -0500

    Starting chapt 2

---
+++ src/main/scala/chapter2.scala
@@ -0,0 +1,31 @@
+//Chapter 2 FP In Scala book
+
+object ChapterTwo {
+
+  //Exercise 1
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(pos: Int, prev: Int, cur: Int): Int =
+      if (pos == 0) prev
+      else go(pos - 1, cur, prev + cur)
+    go(n, 0, 1)
+  }
+
+  //Exercise 2
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int, prev: A) = {
+      if(n == as.length) true
+      else if(gt(as(n), prev)) loop(n + 1, as(n))
+      else false
+    }
+    if(as.length == 0) true
+    else loop(1, as(0))
+  }
+
+  //Exercise 3
+
+
+
+
+}
